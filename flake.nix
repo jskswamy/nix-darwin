852ccf04ce223e277a3ddc0ad9487144518c57fd
Krishnaswamy Subramanian <jskswamy@gmail.com>
2025-04-08 13:37:10 +0530

    Update flake.lock with new dependency revisions
    These updates ensure that the project is using the
    latest compatible versions of its dependencies, improving stability
    and security.

--- flake.nix
+++ flake.nix
@@ -85,7 +85,7 @@
           pkgs.nssTools
           pkgs.ollama
           pkgs.openssh
-          pkgs.open-webui
+          #pkgs.open-webui
           pkgs.pet
           pkgs.pika
           pkgs.pinentry-curses
